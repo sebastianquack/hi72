e945f3c8a310db413037447c658667df12209e60
Sebastian Quack <sebastianquack@gmail.com>
2014-01-05 23:05:13 +0100

    all new poster effects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,8 +40,8 @@ function init_poster_generator() {
 
 	// setup video
 	var	video = document.createElement("video");
-	video.width = canvas.width;
-	video.height = canvas.height;
+	video.width = canvas.width - 20;
+	video.height = canvas.height - 20;
 
 	/* this is taken over by headtracking library
 	var videoObj = { "video": true };
@@ -78,6 +78,11 @@ function init_poster_generator() {
 	frame_context.translate(video.width, 0);
 	frame_context.scale(-1, 1);	    
 
+	var disaster_canvas = document.createElement('canvas');
+	disaster_canvas.width = canvas.width;
+	disaster_canvas.height = canvas.height;
+	var disaster_context = disaster_canvas.getContext("2d");
+
 	var mask_offset_x = 0;
 	var mask_offset_y = 0;
 
@@ -97,17 +102,46 @@ function init_poster_generator() {
 		return obj;
 	}
 
-	background_images[1] = create_image('HG1_transparent.png');
+	/*
+	<select id="select-background">
+	  <option value="0">None</option>
+	  <option value="1">Hagen</option>
+	  <option value="2">Witten</option>
+	</select>
 
-	disaster_images[1] = create_image('blitz.png');
+	<label>Disaster:</label>
+	<select id="select-disaster">
+	  <option value="0">None</option>
+	  <option value="1">Lightning</option>
+	  <option value="2">Fire</option>
+	  <option value="3">Traffic</option>
+	</select>
 
-	mask_images[2] = create_image('Bauhelm_silo.png');
-/*	mask_images[3] = create_image('Hammer_silo.png');
-	mask_images[4] = create_image('xray_silo.png');*/
 
-	silhouette_images[2] = create_image('Bauhelm_kl.png');
-/*	silhouette_images[3] = create_image('Hammer_kl.png');
-	silhouette_images[4] = create_image('xray_kl.png');*/
+	<label>You:</label>
+	<select id="select-silhouette">
+	  <option value="0">None</option>
+	  <option value="1">Eye in the Sky</option>
+	  <option value="2">Xray Glasses</option>
+	  <option value="3">Drill and Hat</option>
+	  <option value="4">Action Hammer</option>
+	</select>
+*/
+
+	background_images[1] = create_image('cities/HG1_transparent.png');
+	background_images[2] = create_image('cities/background_2_transparent.png');
+
+	disaster_images[1] = create_image('disasters/blitz.png');
+	disaster_images[2] = create_image('disasters/feuer.png');
+	disaster_images[3] = create_image('disasters/traffijam_klkl.png');
+
+	mask_images[2] = create_image('silhouettes/xray_silo.png');
+	mask_images[3] = create_image('silhouettes/Bauhelm_silo.png');
+	mask_images[4] = create_image('silhouettes/Hammer_silo.png');
+
+	silhouette_images[2] = create_image('silhouettes/xray_kl.png');
+	silhouette_images[3] = create_image('silhouettes/Bauhelm_kl.png');
+	silhouette_images[4] = create_image('silhouettes/Hammer_kl.png');
 
 	// calculate and display live video effects
 	function apply_effects() {
@@ -123,32 +157,46 @@ function init_poster_generator() {
 
 		if(silhouette_id > 1) {	
 			// prepare masking effect
-			context.drawImage(mask_images[silhouette_id], mask_offset_x, mask_offset_y);
+			context.drawImage(mask_images[silhouette_id], 10 + mask_offset_x, 10 + mask_offset_y);
 			context.globalCompositeOperation = 'source-atop';
 		
 			// add video image to canvas
-			context.drawImage(frame_canvas, 0, 200);
+			context.drawImage(frame_canvas, 5, 200);
 			
 			context.globalCompositeOperation = 'destination-over';
-			context.drawImage(silhouette_images[silhouette_id], mask_offset_x, mask_offset_y);
+			context.drawImage(silhouette_images[silhouette_id], 10 + mask_offset_x, 10 + mask_offset_y);
 		}
 		
-		if(disaster_id > 0) {
+		/*
+		// add disaster
+		if(disaster_id != 3) {
 			context.globalCompositeOperation = 'destination-over';
 			context.drawImage(disaster_images[disaster_id], 0, 0);
-		}
-				
+		}*/
+		
+		// add city		
 		if(background_id > 0) {
 			context.globalCompositeOperation = 'destination-over';
-			context.drawImage(background_images[background_id], 0, 0);
+			context.drawImage(background_images[background_id], 10, 10);
 		}
 		
 		if(silhouette_id == 1) {	
 			// add video image to canvas under everything as godzilla mode
 			context.globalCompositeOperation = 'destination-over';
-			context.drawImage(frame_canvas, 0, 0);			
+			context.drawImage(frame_canvas, 10, 10);			
 		}
-
+		
+		if(disaster_id == 3) {
+			context.globalCompositeOperation = 'source-over';
+			context.drawImage(disaster_images[disaster_id], 0, 0);
+		} else if(disaster_id == 1) {
+			disaster_context.drawImage(disaster_images[disaster_id], 0, 0);
+			disaster_context.blendOnto(context,'lighten');
+		} else if(disaster_id == 2) {
+			disaster_context.drawImage(disaster_images[disaster_id], 0, 0);
+			disaster_context.blendOnto(context,'overlay');
+		}
+		
 	    // Repeat at 30 fps
 	    setTimeout(apply_effects, 1000/30);
 	}
@@ -207,11 +255,11 @@ function init_poster_generator() {
 
 		context.fillStyle = $('#title').css('color');
 		context.font = $('#title').css('font');
-		context.fillText($('#title').val(), x, 325);
+		context.fillText($('#title').val(), x, 425);
 
 		context.fillStyle = $('#subtitle').css('color');
 		context.font = $('#subtitle').css('font');
-		context.fillText($('#subtitle').val(), x, 370);
+		context.fillText($('#subtitle').val(), x, 470);
 
 		$('#titles').hide();
 		$('.selectors').hide();
@@ -267,20 +315,23 @@ function init_poster_generator() {
 
 			$('#posters').show();
 	 		$('#generator').hide();
+			$('li a.generator-link').removeClass('active');
+			$('li a.gallery-link').addClass('active');
+			$('.prev, .next').show();
 						
 		 });	
 	});
 }		
 
 var positionOwlItem = function() {
-/*	if ($(window).width() > 900 && $(window).width() <= 1200) {
+	if ($(window).width() > 900 && $(window).width() <= 1200) {
 		//console.log($(".owl-item").first().width());
 		w = Math.round($(".owl-item").first().width() / 2);
 		$(".owl-item").css("left", w + "px");
 	}
 	else {
 		$(".owl-item").css("left","0");
-	}*/
+	}
 }
 
 $(document).ready(function() {
@@ -301,25 +352,29 @@ $(document).ready(function() {
 		
 	$('html').css('overflow', 'auto');		
 	
-	$('.navigation li a').click(function() {
+	$('.navigation li a').click(function(event) {
 		if($('#front-cover').css('display') == 'block') {
 			$('#front-cover').fadeOut(800);
 			$('#content').show();
-			$('html').css('overflow', 'auto');						
 		}
+		
+		$('.navigation li a').removeClass('active');
+		$(event.target).addClass('active');
+		
+		$('.prev, .next').hide();
 	});
 			
 	$('#front-cover').click(function() {
 		$('#front-cover').fadeOut(800);
 		$('#posters').show();
 		$('#content').show();
-		$('html').css('overflow', 'auto');		
 	});
 
 	$('.gallery-link').click(function(event) {
 		event.preventDefault();
 		$('#posters').show();
 		$('#generator').hide();
+		$('.prev, .next').show();
 	});
 
 	$('.generator-link').click(function(event) {	
@@ -330,23 +385,30 @@ $(document).ready(function() {
 		}
 		$('#posters').hide();
 		$('#generator').show();
-				
+		$('.prev, .next').hide();				
+		$('li a.generator-link').addClass('active');
 	});
 	
 	$('.front-link').click(function() {
 		$('#front-cover').fadeIn();
 		$('#posters').hide();
 		$('#content').hide();
+		$('.navigation li a').removeClass('active');
 	});
 
 	$('a').smoothScroll({
 		offset: -100
 	});
 
-	/*
-	owl.next()   // Go to next slide
-	owl.prev()   // Go to previous slide
-	*/
+	$('.prev').click(function(event) {
+		event.preventDefault();
+		owl.prev()   // Go to previous slide	
+	});
+	
+	$('.next').click(function(event) {
+		event.preventDefault();
+		owl.next()   // Go to next slide
+	});
 	
 });
 
